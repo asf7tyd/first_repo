d3aaaa6557c3f8dda892afb5002eb6d51caca2be
asf7tyd <illromanoff@mail.ru>
2023-03-03 20:28:52 +0300

    lesson

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
-# This is a sample Python script.
+#
+# x=10.5
+# y=15.5
+# my_veriable=1000
+# print(y)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    """
-
-    :rtype: object
-    """
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-print("илья")
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Друг')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# print(type (x))
+x = int(input("press"))
+y = int(input("press numbers"))
+print(x + y)
+print(x - y)
+print(x * y)
