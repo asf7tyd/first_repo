1e278759b9b06b624973502d03a83b50e5dc55ff
asf7tyd <illromanoff@mail.ru>
2023-03-03 21:33:24 +0300

    lesson2

---
+++ lesson2.py
@@ -0,0 +1,12 @@
+n1 = float(input("Число"))
+n2 = float(input("Второе число"))
+if n1 == n2:
+    print("Они равны")
+elif n1 < n2:
+    print(n1, n2)
+elif n1 > n2:
+    print(n1, n2)
+else:
+    n1 > n2
+print(n2)
+print("Они не равны")
