6c132886f7b39856fd021a0880ed366124a6cea3
asf7tyd <illromanoff@mail.ru>
2023-03-14 19:43:32 +0300

    LESSON 5

--- less5.py
+++ less5.py
@@ -1,6 +1,7 @@
-def main():
-    poss
-
-
-if _name_ ='_main_';
-main()
+n = int(input("Введите число"))
+if n > 0:
+    print("Положительное число")
+elif n < 0:
+    print("Отрицательное число")
+else:
+    print("0")
