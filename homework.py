76a8f8d8cf734f0b2aef45bbe8a89e263b9b74d2
asf7tyd <illromanoff@mail.ru>
2023-03-14 19:02:00 +0300

    3\14

--- homework.py
+++ homework.py
@@ -1,16 +1,27 @@
-# №1
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# max_number = max(numbers)
-# min_number = min(numbers)
-
-# print("Максимальное число:", max_number)
-# print("Минимальное число:", min_number)
-# for num in numbers:
-#  if num % 3 == 0 and num % 5 != 0:
-#     print(num)
+# №№1
+# numbers = input("Введите числа через пробел")
+# numbers_list = numbers.split()
+# numbers_list.reverse()
+# print(numbers_list)
 # №2
-# n = [1, 2, 3, 4, 5, 2, 3, 4, 5]
-# for num in n:
-#   if n.count(num) == 1:
-#     print(num)
-#     break
+months = {1: ('Январь', 31),
+          2: ('Февраль', 28),
+          3: ('Март', 31),
+          4: ('Апрель', 30),
+          5: ('Май', 31),
+          6: ('Июнь', 30),
+          7: ('Июль', 31),
+          8: ('Август', 31),
+          9: ('Сентябрь', 30),
+          10: ('Октябрь', 31),
+          11: ('Ноябрь', 30),
+          12: ('Декабрь', 31)
+
+          }
+
+month_number = int(input("Введите номер месяца: "))
+if month_number in months:
+    month_name, days_count = months[month_number]
+    print(f"В месяце {month_name} {days_count} дней.")
+else:
+    print("Некорректный номер месяца.")
