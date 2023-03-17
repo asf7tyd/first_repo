47b6e8d4fef7daff650963a05a70b572e5d665e5
asf7tyd <illromanoff@mail.ru>
2023-03-17 17:26:15 +0300

    homework 17/03 #1

--- less5.py
+++ less5.py
@@ -1,7 +1,20 @@
-n = int(input("Введите число"))
-if n > 0:
-    print("Положительное число")
-elif n < 0:
-    print("Отрицательное число")
-else:
-    print("0")
+def calculate(num1, num2, operator):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        return num1 / num2
+    else:
+        return "Invalid operator"
+
+
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+operator = input("Введите оператор  (+, -, *, /): ")
+
+result = calculate(num1, num2, operator)
+
+print(f"{num1} {operator} {num2} = {result}")
