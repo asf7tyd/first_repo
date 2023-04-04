d441cd4ad4a532d293a5f49d920b38542baa6549
asf7tyd <illromanoff@mail.ru>
2023-04-04 18:50:22 +0300

    3/24/2023

---
+++ Telegram bot/telebot.py
@@ -0,0 +1,13 @@
+import telebot;
+
+bot = telebot.TeleBot('6100378539:AAHXzYi15rMy3MquLMl12m1DnizYmoQsKQo');
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
